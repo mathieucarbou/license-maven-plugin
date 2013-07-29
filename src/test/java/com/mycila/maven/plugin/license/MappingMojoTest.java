f19aca66ffd6cb4da0fbe1bcd139c377252c1e8c
mathieucarbou <mathieu.carbou@gmail.com>
2013-07-29 15:44:43 -0400

    licenses

--- src/test/java/com/mycila/maven/plugin/license/MappingMojoTest.java
+++ src/test/java/com/mycila/maven/plugin/license/MappingMojoTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2008 http://code.google.com/p/maven-license-plugin/
+ * Copyright (C) 2008 Mycila (mathieu.carbou@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.mycila.maven.plugin.license;
 
 import org.apache.maven.monitor.logging.DefaultLog;
