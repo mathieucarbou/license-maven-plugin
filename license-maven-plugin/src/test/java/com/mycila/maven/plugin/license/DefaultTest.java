d70e711e4071d6a47afb53f0bb9b1f0e6cdb4e26
Peter Lynch <plynch@sonatype.com>
2021-04-17 17:14:01 -0300

    Add test

---
+++ license-maven-plugin/src/test/java/com/mycila/maven/plugin/license/DefaultTest.java
@@ -0,0 +1,18 @@
+package com.mycila.maven.plugin.license;
+
+import java.util.Arrays;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class DefaultTest
+{
+  @Test
+  public void test_Jenkinsfile_is_not_a_default_exclude() {
+    assertThat(Arrays.asList(Default.EXCLUDES), not(CoreMatchers.<String>hasItems(containsString("Jenkinsfile"))));
+  }
+}
